dabc307adc48f263fa737de16e21dabffdc13553
zhangyu <1802833394@qq.com>
2023-07-11 13:47:39 +0800

    完善员工添加功能1，相同的username，在全局异常处理类中进行捕获，返回给前端相应的信息

--- sky-take-out/sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
+++ sky-take-out/sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
@@ -1,10 +1,14 @@
 package com.sky.handler;
 
+import com.sky.constant.MessageConstant;
 import com.sky.exception.BaseException;
 import com.sky.result.Result;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.sql.SQLIntegrityConstraintViolationException;
+import java.sql.SQLNonTransientException;
 
 /**
  * 全局异常处理器，处理项目中抛出的业务异常
@@ -19,9 +23,25 @@ public class GlobalExceptionHandler {
      * @return
      */
     @ExceptionHandler
-    public Result exceptionHandler(BaseException ex){
+    public Result exceptionHandler(BaseException ex) {
         log.error("异常信息：{}", ex.getMessage());
         return Result.error(ex.getMessage());
     }
 
+    /**
+     * 数据库异常
+     * @param ex
+     * @return
+     */
+    @ExceptionHandler
+    public Result exceptionHandler(SQLIntegrityConstraintViolationException ex) {
+        String msg = ex.getMessage();
+        if (msg.contains("Duplicate entry")) {
+            String[] split = msg.split(" ");
+            String username = split[2];
+            return Result.error(username + MessageConstant.ALREADY_EXISTS);
+        } else {
+            return Result.error(MessageConstant.UNKNOWN_ERROR);
+        }
+    }
 }
