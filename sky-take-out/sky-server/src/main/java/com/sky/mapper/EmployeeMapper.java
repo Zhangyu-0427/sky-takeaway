9f59d26a32ac63ba42755e5adb91f6e82f504de2
zhangyu <1802833394@qq.com>
2023-07-12 10:56:05 +0800

    自定义注解+自定义切面在Mapper层实现公共字段填充

--- sky-take-out/sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-take-out/sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,8 +1,11 @@
 package com.sky.mapper;
 
 import com.github.pagehelper.Page;
+import com.sky.annotation.AutoFill;
 import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
+import com.sky.enumeration.OperationType;
+import jdk.internal.org.objectweb.asm.tree.analysis.Value;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
@@ -24,6 +27,7 @@ public interface EmployeeMapper {
      *
      * @param employee
      */
+    @AutoFill(value = OperationType.INSERT)
     @Insert("insert into employee(name, username, password, phone, sex, id_number, status, create_time, update_time, create_user, update_user) " +
             "values " +
             "(#{name}, #{username}, #{password}, #{phone}, #{sex}, #{idNumber}, #{status}, #{createTime}, #{updateTime}, #{createUser}, #{updateUser})")
@@ -42,6 +46,7 @@ public interface EmployeeMapper {
      *
      * @param employee
      */
+    @AutoFill(value = OperationType.UPDATE)
     void update(Employee employee);
 
     /**
