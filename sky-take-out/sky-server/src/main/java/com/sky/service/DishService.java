08aafcfa46c25cdf000a07d82bee519beb7e8678
zhangyu <1802833394@qq.com>
2023-07-13 13:40:17 +0800

    删除菜品业务代码开发

--- sky-take-out/sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-take-out/sky-server/src/main/java/com/sky/service/DishService.java
@@ -4,6 +4,8 @@ import com.sky.dto.DishDTO;
 import com.sky.dto.DishPageQueryDTO;
 import com.sky.result.PageResult;
 
+import java.util.List;
+
 public interface DishService {
 
 
@@ -16,8 +18,16 @@ public interface DishService {
 
     /**
      * 菜品分页查询
+     *
      * @param dishPageQueryDTO
      * @return
      */
     PageResult pageQuery(DishPageQueryDTO dishPageQueryDTO);
+
+    /**
+     * 菜品批量删除
+     *
+     * @param ids
+     */
+    void deleteBatch(List<Long> ids);
 }
