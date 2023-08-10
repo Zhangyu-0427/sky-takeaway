93674d6b029fa06901ee4f7c2047116c692b76fa
zhangyu <1802833394@qq.com>
2023-08-10 17:23:30 +0800

    用户查询历史订单功能代码开发

--- sky-take-out/sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-take-out/sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -1,5 +1,7 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
+import com.sky.dto.OrdersPageQueryDTO;
 import com.sky.entity.Orders;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
@@ -9,12 +11,14 @@ public interface OrderMapper {
 
     /**
      * 插入订单
+     *
      * @param orders
      */
     void insert(Orders orders);
 
     /**
      * 根据订单号查询订单
+     *
      * @param orderNumber
      */
     @Select("select * from orders where number = #{orderNumber}")
@@ -22,7 +26,15 @@ public interface OrderMapper {
 
     /**
      * 修改订单信息
+     *
      * @param orders
      */
     void update(Orders orders);
+
+    /**
+     * 分页条件查询并按下单时间排序
+     *
+     * @param ordersPageQueryDTO
+     */
+    Page<Orders> pageQuery(OrdersPageQueryDTO ordersPageQueryDTO);
 }
